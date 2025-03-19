32a1aa7851f9f74697c9ea17a1b8474260118670
Gaytri Dodiya <dodiyadsvv@gmail.com>
2025-03-19 23:32:24 +0530

    Added portfolio website files

---
+++ src/data/constants.jsx
@@ -0,0 +1,268 @@
+import gosoft from '../images/gosoft.png';
+import techpanda from '../images/techpanda.jpeg';
+import company from '../images/company.jpg';
+import dsvv from '../images/dsvv.jpg';
+export const Bio = {
+  name: 'Gaytri Dodiya',
+  roles: ['Web Developer', 'Front End Developer', 'WordPress Developer'],
+  description:
+    'Detail-oriented Front End Developer with a strong foundation in JavaScript, React, HTML5, and CSS3.Experienced in developing responsive and user-friendly web applications. Proven ability to collaborate in team environments and deliver high-quality coding solutions. Recognized for strong problem-solving skills and a commitment to continuous learning',
+  github: 'https://github.com/GaytriDodiya',
+  resume:
+    'https://drive.google.com/file/d/1o57ALsfqJ9L9HF6F61hTdLzhiSRRltOt/view?usp=drive_link',
+  linkedin: 'https://www.linkedin.com/in/gaytri-dodiya-295aa6216/',
+  // twitter: 'https://twitter.com/RishavChanda',
+  // insta: 'https://www.instagram.com/rishav_chanda/',
+  // facebook: 'https://www.facebook.com/rishav.chanda.165/',
+};
+
+export const skills = [
+  {
+    title: 'Frontend',
+    skills: [
+      {
+        name: 'React Js',
+        image:
+          'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
+      },
+      // {
+      //   name: 'Redux',
+      //   image:
+      //     'https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg',
+      // },
+      // {
+      //   name: 'Next Js',
+      //   image:
+      //     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=',
+      // },
+      // {
+      //   name: 'Angular Js',
+      //   image:
+      //     'https://camo.githubusercontent.com/8886130b3d8aba95dbdd7c4f9a41029606424cc06d1873c1ced87dd55a222fef/68747470733a2f2f616e67756c61722e696f2f6173736574732f696d616765732f6c6f676f732f616e67756c61722f616e67756c61722e737667',
+      // },
+      {
+        name: 'HTML',
+        image: 'https://www.w3.org/html/logo/badge/html5-badge-h-solo.png',
+      },
+      {
+        name: 'CSS',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png',
+      },
+      {
+        name: 'JavaScript',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png',
+      },
+      {
+        name: 'Bootstrap',
+        image:
+          'https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png',
+      },
+
+      // {
+      //   name: 'Flutter',
+      //   image:
+      //     'https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png',
+      // },
+    ],
+  },
+
+  {
+    title: 'Others',
+    skills: [
+      {
+        name: 'GitHub',
+        image:
+          'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+      },
+      {
+        name: 'Netlify',
+        image:
+          'https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png',
+      },
+      {
+        name: 'VS Code',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519',
+      },
+      {
+        name: 'Postman',
+        image:
+          'https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png',
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: gosoft,
+    role: 'Web Developer',
+    company: 'GoSoft Informatics Pvt. Ltd',
+    date: 'March 2023- May 2024',
+    desc: 'Developed a responsive web application, RoonBerg, for an international client. Collaborated with UI/UX designers to implement design enhancements and ensure a seamless user experience. Optimized web pages for maximum speed, performance, and scalability..',
+    skills: [
+      'ReactJS',
+      'Redux',
+      'NodeJs',
+      'Material UI',
+      'HTML',
+      'CSS',
+      'JavaScript',
+    ],
+    doc: 'https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Screenshot%20from%202024-01-25%2022-38-31.png?alt=media&token=2785903f-1a4e-41f5-afd2-6adcfe56d058',
+  },
+  {
+    id: 1,
+    img: techpanda,
+    role: 'Front End Intern',
+    company: 'TechPanda Pvt. Ltd.',
+    date: 'December 2022 - February 2023',
+    desc: 'Worked on a live project alongside a senior developer, contributing to the development and modification of webpages for a blood donation website under a Rajasthan government project. Assisted in implementing changes, improving UI/UX, and ensuring the website functionality met project requirements.',
+    skills: ['Wire Frame', 'PHP', 'CSS', 'Laravel', 'JavaScript', 'Html'],
+    doc: 'https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/1696514649120.jpeg?alt=media&token=e7f6757b-edfa-4138-a692-d6709eeef3e2',
+  },
+  {
+    id: 2,
+    img: company,
+    role: 'Freelancing',
+    company: 'Alpha Web Development',
+    date: 'June 2022 - September 2022',
+    desc: 'Collaborated with a team to develop an e-commerce website with web services integration. Contributed to the development of web pages, integrated the payment gateway, and implemented user login, registration, and cart sections. Additionally, provided support for database queries to ensure seamless backend functionality.',
+    skills: [
+      'Html',
+      'Css',
+      'JavaScript',
+      'Core Php',
+      'Xmap',
+      'DataBase',
+      'Vs Code',
+    ],
+    doc: 'https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245',
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: dsvv,
+    school: 'Dev Sanskriti Vishwavidyalaya',
+    date: 'June 2020 - June 2022',
+    grade: '8.05 CGPA',
+    desc: "I hold a Master's degree in Computer Science from Dev Sanskriti Vishwavidyalaya, Haridwar (UK). I completed the 2-year integrated MCA program with a CGPA of 8.05.",
+    degree: ' Master Of Computer Application(MCA), Computer Science ',
+  },
+  {
+    id: 1,
+    img: dsvv,
+    school: 'Dev Sanskriti Vishwavidyalaya',
+    date: 'Apr 2017 - Apr 2020',
+    grade: '6.93 CGPA',
+    desc: "I hold Bachler's degree in Computer Science from Dev Sanskriti Vishwavidyalaya, Haridwar (UK). I completed the 3-year BCA program with a CGPA of 6.93.",
+    degree: 'Bachler Of Computer Application(BCA), Computer Science',
+  },
+  // {
+  //   id: 2,
+  //   img: 'https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3',
+  //   school: 'Methodist School, Dankuni',
+  //   date: 'Apr 2017 - Apr 2019',
+  //   grade: '92.3%',
+  //   desc: 'I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.',
+  //   degree: 'ICSC(X), Science with Computer',
+  // },
+];
+
+export const projects = [
+  // {
+  //   id: 11,
+  //   title: 'DecisionHub',
+  //   date: 'Jan 2024 - Dec 2023',
+  //   description:
+  //     'A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and visualize decision strategies. Provide a no-code rule writing experience and visual representation to test these rules in real-time and observe the calculations at each step.',
+  //   image:
+  //     'https://github.com/rishavchanda/DecisionHub/raw/master/assets/testRule.jpg',
+  //   tags: [
+  //     'React Js',
+  //     'PostgressSQL',
+  //     'Node Js',
+  //     'Express Js',
+  //     'Redux',
+  //     'React Flow',
+  //   ],
+  //   category: 'web app',
+  //   github: 'https://github.com/rishavchanda/DecisionHub',
+  //   webapp: 'https://decisionhub.netlify.app/',
+  // },
+  // {
+  //   id: 9,
+  //   title: 'Trackify',
+  //   date: 'Jun 2023 - Jul 2023',
+  //   description:
+  //     "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+  //   image:
+  //     'https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png',
+  //   tags: [
+  //     'Docker',
+  //     'AWS',
+  //     'DuckDNS',
+  //     'Eslint',
+  //     'Husky',
+  //     'CI/CD',
+  //     'React Js',
+  //     'MongoDb',
+  //     'Node Js',
+  //     'Express Js',
+  //     'Redux',
+  //   ],
+  //   category: 'web app',
+  //   github: 'https://github.com/rishavchanda/Trackify',
+  //   webapp: 'https://trackify-management.netlify.app/',
+  // },
+  {
+    id: 0,
+    title: 'Amazona Cloan',
+    description:
+      'Developed a full-stack web application that allows users to search, add to Card, Buy their favorite Products on website demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices.',
+    image:
+      'https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png',
+    tags: ['React Js', 'MongoDb', 'Node Js', 'Express Js', 'Redux'],
+    category: 'web app',
+    github: 'https://github.com/rishavchanda/Podstream',
+    webapp: 'https://podstream.netlify.app/',
+    // member: [
+    //   {
+    //     name: 'Rishav Chanda',
+    //     img: 'https://avatars.githubusercontent.com/u/64485885?v=4',
+    //     linkedin: 'https://www.linkedin.com/in/rishav-chanda-b89a791b3/',
+    //     github: 'https://github.com/rishavchanda/',
+    //   },
+    //   {
+    //     name: 'Upasana Chaudhuri',
+    //     img: 'https://avatars.githubusercontent.com/u/100614635?v=4',
+    //     linkedin: 'https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/',
+    //     github: 'https://github.com/upasana0710',
+    //   },
+    // ],
+  },
+  {
+    id: 1,
+    title: 'SocialWaves',
+    description:
+      'Designed and developed a social media-inspired website inspired by Facebook, enabling users to register, follow/unfollow, like posts, and make friends. Implemented user authentication, profile management, and interactive features to enhance user engagement.',
+    image:
+      'https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png',
+    tags: [
+      'React Js',
+      'MongoDb',
+      'Node Js',
+      'Express Js',
+      'Redux',
+      'NodeMailer',
+    ],
+    category: 'web app',
+    github: 'https://github.com/rishavchanda/Project-Management-App',
+    webapp: 'https://vexa-app.netlify.app/',
+  },
+];
